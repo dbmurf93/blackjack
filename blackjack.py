be6964436014481d7bbfdd89d9723b0f60f22086
Dylan Murphy <dbmurf93@gmail.com>
2020-10-11 16:40:11 -0500

    working through basic structure for blackjack

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,8 @@ class Player(object):
     '''  Represents a player with a name, balance, and hand_history list '''
     def __init__(self, name, balance = 50, hand_history = []):
         self.name = str(name)
+        self.balance = balance
+        self.hand_history = hand_history
 
     def get_name(self): 
         return self.name
@@ -9,7 +11,16 @@ class Player(object):
     def get_balance(self):
         return self.balance
     
-    def add_balance(self, amt):
+    def add_to_balance(self, amt):
+        ''' takes amt argument as a positive int '''
+        # balance = self.get_balance()
+        # balance += amt
+        self.balance += amt
+   
+
+    def make_bet(self, bet):
+        self.balance -= bet
+
 
 
 
@@ -20,7 +31,7 @@ def blackjack(self, players_list):
     Starts a new game with up to 3 players sitting across from the dealer
     Returns players_dict with updated balances
     '''
-
+    pass
 
 
 def build_players_list(self):
@@ -28,31 +39,52 @@ def build_players_list(self):
     - Takes input from users, builds ((dict or list)) with up to ## players
     - Each player
     '''
+    pass
 
 
 
 
 def check_keep_playing(self, players_list):
-'''
-Asks each player if they want to play again and reports their results
-'''
-    for player in players_list.keys():
-        balance = player.get_balance()
-        if balance > start_balance: 
-            win_or_lose = 'win'
-        elif balance == start_balance: 
-            win_or_lose = 'broke even'
-        elif balance < start_balance: 
-            loss_amt = start_balance - balance
-            win_or_lose = 'lost {loss_amt}'
-        print('Good Game {player}! You {win_or_lose}.')
+    '''
+    Asks each player if they want to play again, reports their results
+    returns whether the game should start again, as well as whos playing
+    '''
+    
+    for player in players_list:
+        balance = player.get_balance 
+        name = player.get_name()
+        win_amt = balance - ref_balance
+        
+        if balance > ref_balance: 
+            win_or_lose = 'Won ${win_amt}'
+
+        elif balance == ref_balance: 
+            win_or_lose = 'broke even.'
+
+        elif balance < ref_balance: 
+            loss = abs(win_amt)
+            win_or_lose = 'lost ${loss}'
+
+        print('{name}, You {win_or_lose} this hand.')
         ans = input('Play again? (y/n)').lower().strip()
-        if ans == 'y' or ans == 'yes': continue
+        
+        if ans == 'y' or ans == 'yes': 
+            bet = int(input("Enter bet..."))
+            continue #next player
         elif ans == 'n' or ans == 'no':
-            player_balance = get_player_balance()
+            player_balance = player.get_player_balance()
             if player_balance == 0:
                 print('Sorry to see you go, thanks for playing!')
-            else: print('Sorry to see you go thanks for the ${player_balance}!')
+            else: 
+                print('Sorry to see you go thanks for the ${player_balance}!')
+            
+            players_list.remove(player)
+            continue
+
+        else: print("I'll just pretend I understood that, you will play again")
+
+    return players_list
+
 
 
 
@@ -61,9 +93,14 @@ Asks each player if they want to play again and reports their results
 
 if __name__ == "__main__":
     keep_playing = True
-    players_dict = build_players_dict()
+    players_list = build_players_list()
     
     while keep_playing:
-        blackjack(players_dict)
-        check_keep_playing()
-        
+        blackjack(players_list)
+        players_list = check_keep_playing(players_list) #returns players list based on who wants to play again, any leftover money is donated back to house
+        if len(players_list) == 0: 
+            print('Bye for now!')
+            break
+        else: 
+            print('Starting new game with {players_list}'
+            continue
